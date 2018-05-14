e66a71c936f369b822ef8575d56cb91c62e2a11c
ES-SARRAJ <essarraj.fouad@gmail.com>
2018-05-14 12:47:02 +0200

    Ajoutez des fichiers projet.

---
+++ PagingAndSorting/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using PagingAndSorting.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace PagingAndSorting.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<EmployeeMaster> Employees { get; set; }
+    }
+}
